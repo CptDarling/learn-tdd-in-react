55198f121f993513e24853bccb254f3d32cc334e
Richard Holyoak <rholyoak@gmail.com>
2023-03-03 12:35:36 +0000

    Add display of messages history

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import NewMessageForm from "./components/NewMessageForm";
+import MessageList from './components/MessageList';
+
 export default function App() {
+  const [messages, setMessages] = useState([]);
+
+  function handleSend(newMessage) {
+    setMessages([newMessage, ...messages]);
+  }
+
   return (
+    <>
       <NewMessageForm onSend={handleSend} />
+      <MessageList data={messages} />
+    </>
   )
 }
